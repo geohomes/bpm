593496c60259751f3a5d4709a897a76fce2389c8
bestproperty <global.bestpropertymarket@gmail.com>
2022-04-22 15:42:50 +0100

    ##

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -41,15 +41,9 @@ class UsersSeeder extends Seeder
             ];
 
             User::factory()->count(312)->create();
-            foreach ($users as $user) {
-                $user = User::where([
-                    'email' => $user['email'], 
-                    'phone' => $user['phone']
-                ])->first();
-
-                empty($user) ? '' : $user->delete();
-                User::create($user);
-            }
+            // foreach ($users as $user) {
+            //     User::create($user);
+            // }
 
         }
     }
